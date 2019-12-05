c81572305c95a458e491163bf055c3a431ece0d2
kredmond <kelsu02@gmail.com>
2019-12-05 14:49:41 -0700

    Created boxes

--- answer.js
+++ answer.js
@@ -1,1 +1,21 @@
 // *** YOUR ANSWER BELOW ***
+function drawBox(width, height) {
+    let corner = "+";
+    for (let i = 1; i <= height; i++) {
+        let horizon = "";
+        let vertical = "|";
+        if (i === 1 || i === height) {
+            for (let j = 2; j < width; j++) {
+                horizon += "-";
+            } 
+            console.log(corner + horizon + corner);
+        } else {
+            for (let j = 2; j < width; j++) {
+                horizon += " ";
+            } 
+            console.log(vertical + horizon + vertical);
+        }
+    }
+}
+
+drawBox(52,6);
